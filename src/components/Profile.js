09ff3b86ff19b278257d8f32a7e47ce2d4e5c06f
Alex Corey <alex.swansboro@gmail.com>
2018-11-05 07:53:44 -0500

    comitting to deploy

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -14,23 +14,25 @@ class Profile extends Component {
     return (<div>
       <form>
         <Label>Enter Your Work Schedule</Label>
+        <div />
         <Input placeholder='What time of day do you leave for work' />
+        <div />
         <Input placeholder='What time of day do you get home from work' />
         <div className='weekDay-selector'>
-          <Input value='Sunday' type='checkbox' id='weekDay-sun' class='weekday' />
-          <Label for='weekDay-sun'>S</Label>
-          <Input value='Monday' type='checkbox' id='weekDay-mon' class='weekday' />
-          <Label for='weekDay-mon'>M</Label>
-          <Input value='Tuesday' type='checkbox' id='weekDay-tue' class='weekday' />
-          <Label for='weekDay-tue'>T</Label>
-          <Input value='Wednesday' type='checkbox' id='weekDay-wed' class='weekday' />
-          <Label for='weekDay-wed'>W</Label>
-          <Input value='Thursday' type='checkbox' id='weekDay-thu' class='weekday' />
-          <Label for='weekDay-thur'>T</Label>
-          <Input value='Friday' type='checkbox' id='weekDay-fri' class='weekday' />
-          <Label for='weekDay-fri'>F</Label>
-          <Input value='Saturday' type='checkbox' id='weekDay-sat' class='weekday' />
-          <Label for='weekDay-sat'>S</Label>
+          <Input value='Sunday' type='checkbox' id='weekDay-sun' className='weekday' />
+          <Label htmlFor='weekDay-sun'>S</Label>
+          <Input value='Monday' type='checkbox' id='weekDay-mon' className='weekday' />
+          <Label htmlFor='weekDay-mon'>M</Label>
+          <Input value='Tuesday' type='checkbox' id='weekDay-tue' className='weekday' />
+          <Label htmlFor='weekDay-tue'>T</Label>
+          <Input value='Wednesday' type='checkbox' id='weekDay-wed' className='weekday' />
+          <Label htmlFor='weekDay-wed'>W</Label>
+          <Input value='Thursday' type='checkbox' id='weekDay-thu' className='weekday' />
+          <Label htmlFor='weekDay-thur'>T</Label>
+          <Input value='Friday' type='checkbox' id='weekDay-fri' className='weekday' />
+          <Label htmlFor='weekDay-fri'>F</Label>
+          <Input value='Saturday' type='checkbox' id='weekDay-sat' className='weekday' />
+          <Label htmlFor='weekDay-sat'>S</Label>
         </div>
       </form>
     </div>)
